d7e180a31fae3a1e79c771b9a30c046cf2050977
dzx666 <3020713795@qq.com>
2023-07-30 13:30:24 +0800

    修正是否可跳过填写错误

--- 事件索引创建.py
+++ 事件索引创建.py
@@ -12,7 +12,7 @@ for file in os.listdir(path):
         f = open(file_path,"r",encoding="utf-8")
         res = f.readlines()
         f.close()
-        events[int(res[0][:-1])]=[int(res[0][:-1]),file_path,res[1][:-1],res[2][:-1],bool(res[3][:-1]),res[4][:-1]]
+        events[int(res[0][:-1])]=[int(res[0][:-1]),file_path,res[1][:-1],res[2][:-1],bool(eval(res[3][:-1])),res[4][:-1]]
 f = open("events/index.json","w",encoding="utf-8")
 datar = json.dumps(events, sort_keys=True, indent=4, separators=(',', ': '))
 f.write(datar)
